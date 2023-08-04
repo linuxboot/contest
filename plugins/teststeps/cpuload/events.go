42d4ecd8082c4286ea61b000e470dfc21485d605
llogen <christoph.lange@9elements.com>
2023-08-04 10:43:11 +0200

    feat(plugins/teststeps): add new teststep cpuload
    Signed-off-by: llogen <christoph.lange@9elements.com>

---
+++ plugins/teststeps/cpuload/events.go
@@ -0,0 +1,116 @@
+package cpuload
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/linuxboot/contest/pkg/event"
+	"github.com/linuxboot/contest/pkg/event/testevent"
+	"github.com/linuxboot/contest/pkg/target"
+	"github.com/linuxboot/contest/pkg/xcontext"
+)
+
+// events that we may emit during the plugin's lifecycle
+const (
+	EventStdout = event.Name("Stdout")
+	EventStderr = event.Name("Stderr")
+)
+
+// Events defines the events that a TestStep is allow to emit. Emitting an event
+// that is not registered here will cause the plugin to terminate with an error.
+var Events = []event.Name{
+	EventStdout,
+	EventStderr,
+}
+
+type eventPayload struct {
+	Msg string
+}
+
+func emitEvent(ctx xcontext.Context, name event.Name, payload interface{}, tgt *target.Target, ev testevent.Emitter) error {
+	payloadData, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("cannot marshal payload for event '%s': %w", name, err)
+	}
+
+	msg := json.RawMessage(payloadData)
+	data := testevent.Data{
+		EventName: name,
+		Target:    tgt,
+		Payload:   &msg,
+	}
+
+	if err := ev.Emit(ctx, data); err != nil {
+		return fmt.Errorf("cannot emit event EventCmdStart: %w", err)
+	}
+
+	return nil
+}
+
+// Function to format teststep information and append it to a string builder.
+func (ts *TestStep) writeTestStep(builders ...*strings.Builder) {
+	for _, builder := range builders {
+		builder.WriteString("Input Parameter:\n")
+		builder.WriteString("  Transport:\n")
+		builder.WriteString(fmt.Sprintf("    Protocol: %s\n", ts.Transport.Proto))
+		builder.WriteString("    Options: \n")
+		optionsJSON, err := json.MarshalIndent(ts.Transport.Options, "", "    ")
+		if err != nil {
+			builder.WriteString(fmt.Sprintf("%v", ts.Transport.Options))
+		} else {
+			builder.WriteString(string(optionsJSON))
+		}
+		builder.WriteString("\n")
+
+		builder.WriteString("  Parameter:\n")
+		builder.WriteString(fmt.Sprintf("    ToolPath: %s\n", ts.Parameter.ToolPath))
+		builder.WriteString(fmt.Sprintf("    Args: %v\n", ts.Parameter.Args))
+		builder.WriteString(fmt.Sprintf("    Cores: %v\n", ts.Parameter.Cores))
+		builder.WriteString("\n")
+
+		builder.WriteString("  Options:\n")
+		builder.WriteString(fmt.Sprintf("    Timeout: %s\n", time.Duration(ts.Options.Timeout)))
+
+		builder.WriteString("Default Values:\n")
+		builder.WriteString(fmt.Sprintf("  Timeout: %s", defaultTimeout))
+
+		builder.WriteString("\n\n")
+
+		builder.WriteString("Expect Parameter:\n")
+		builder.WriteString("  General expectations:\n")
+		for _, expect := range ts.expectStepParams.General {
+			builder.WriteString(fmt.Sprintf("    Option: %s\n", expect.Option))
+			builder.WriteString(fmt.Sprintf("    Value: %s\n", expect.Value))
+		}
+
+		builder.WriteString("  Core specific expectations:\n")
+		for _, expect := range ts.expectStepParams.Individual {
+			builder.WriteString(fmt.Sprintf("  Core %d:\n", expect.Core))
+			builder.WriteString(fmt.Sprintf("    Option: %s\n", expect.Option))
+			builder.WriteString(fmt.Sprintf("    Value: %s\n", expect.Value))
+		}
+		builder.WriteString("\n\n")
+	}
+}
+
+// Function to format command information and append it to a string builder.
+func writeCommand(args string, builders ...*strings.Builder) {
+	for _, builder := range builders {
+		builder.WriteString("Operation on DUT:\n")
+		builder.WriteString(args)
+		builder.WriteString("\n\n")
+
+		builder.WriteString("Output:\n")
+	}
+}
+
+// emitStderr emits the whole error message an returns the error
+func emitStderr(ctx xcontext.Context, name event.Name, stderrMsg string, tgt *target.Target, ev testevent.Emitter, err error) error {
+	if err := emitEvent(ctx, EventStderr, eventPayload{Msg: stderrMsg}, tgt, ev); err != nil {
+		return fmt.Errorf("cannot emit event: %v", err)
+	}
+
+	return err
+}
---
+++ plugins/teststeps/cpuload/runner.go
@@ -0,0 +1,279 @@
+package cpuload
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"time"
+
+	"github.com/insomniacslk/xjson"
+	"github.com/linuxboot/contest/pkg/event/testevent"
+	"github.com/linuxboot/contest/pkg/target"
+	"github.com/linuxboot/contest/pkg/test"
+	"github.com/linuxboot/contest/pkg/xcontext"
+	"github.com/linuxboot/contest/plugins/teststeps/abstraction/transport"
+	"github.com/linuxboot/contest/plugins/teststeps/cpu"
+)
+
+const (
+	supportedProto = "ssh"
+	privileged     = "sudo"
+	cmd            = "cpu"
+	argument       = "stats"
+	jsonFlag       = "--json"
+)
+
+type TargetRunner struct {
+	ts *TestStep
+	ev testevent.Emitter
+}
+
+func NewTargetRunner(ts *TestStep, ev testevent.Emitter) *TargetRunner {
+	return &TargetRunner{
+		ts: ts,
+		ev: ev,
+	}
+}
+
+func (r *TargetRunner) Run(ctx xcontext.Context, target *target.Target) error {
+	var stdoutMsg, stderrMsg strings.Builder
+
+	// limit the execution time if specified
+	var cancel xcontext.CancelFunc
+
+	if r.ts.Options.Timeout != 0 {
+		ctx, cancel = xcontext.WithTimeout(ctx, time.Duration(r.ts.Options.Timeout))
+		defer cancel()
+	} else {
+		r.ts.Options.Timeout = xjson.Duration(defaultTimeout)
+		ctx, cancel = xcontext.WithTimeout(ctx, time.Duration(r.ts.Options.Timeout))
+		defer cancel()
+	}
+
+	pe := test.NewParamExpander(target)
+
+	if r.ts.Transport.Proto != supportedProto {
+		err := fmt.Errorf("only '%s' is supported as protocol in this teststep", supportedProto)
+		stderrMsg.WriteString(fmt.Sprintf("%v", err))
+
+		return emitStderr(ctx, EventStderr, stderrMsg.String(), target, r.ev, err)
+	}
+
+	if r.ts.Parameter.Duration != "" {
+		if _, err := time.ParseDuration(r.ts.Parameter.Duration); err != nil {
+			return fmt.Errorf("wrong interval statement, valid units are ns, us, ms, s, m and h")
+		}
+	}
+
+	r.ts.writeTestStep(&stdoutMsg, &stderrMsg)
+
+	transport, err := transport.NewTransport(r.ts.Transport.Proto, r.ts.Transport.Options, pe)
+	if err != nil {
+		err := fmt.Errorf("failed to create transport: %w", err)
+		stderrMsg.WriteString(fmt.Sprintf("%v", err))
+
+		return emitStderr(ctx, EventStderr, stderrMsg.String(), target, r.ev, err)
+	}
+
+	if err := r.ts.runLoad(ctx, &stdoutMsg, &stderrMsg, transport); err != nil {
+		stderrMsg.WriteString(fmt.Sprintf("%v", err))
+
+		return emitStderr(ctx, EventStderr, stderrMsg.String(), target, r.ev, err)
+	}
+
+	if err := emitEvent(ctx, EventStdout, eventPayload{Msg: stdoutMsg.String()}, target, r.ev); err != nil {
+		return fmt.Errorf("cannot emit event: %v", err)
+	}
+
+	return err
+}
+
+func (ts *TestStep) runLoad(ctx xcontext.Context, stdoutMsg, stderrMsg *strings.Builder, transport transport.Transport,
+) error {
+	var (
+		args []string
+	)
+
+	if len(ts.Parameter.Cores) == 0 {
+		args = []string{"stress-ng", "--cpu $(nproc)", "--cpu-load 100", fmt.Sprintf("--timeout %s", ts.Parameter.Duration), "&"}
+	} else {
+		for _, core := range ts.Parameter.Cores {
+			args = append(args, []string{fmt.Sprintf("taskset -c %d", core), "stress-ng", "--cpu 1", "--cpu-load 100",
+				fmt.Sprintf("--timeout %s", ts.Parameter.Duration), "&"}...)
+		}
+	}
+
+	proc, err := transport.NewProcess(ctx, privileged, args, "")
+	if err != nil {
+		return fmt.Errorf("Failed to create proc: %w", err)
+	}
+
+	writeCommand(proc.String(), stdoutMsg, stderrMsg)
+
+	stdoutPipe, err := proc.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("Failed to pipe stdout: %v", err)
+	}
+
+	stderrPipe, err := proc.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("Failed to pipe stderr: %v", err)
+	}
+
+	// try to start the process, if that succeeds then the outcome is the result of
+	// waiting on the process for its result; this way there's a semantic difference
+	// between "an error occured while launching" and "this was the outcome of the execution"
+	outcome := proc.Start(ctx)
+	if outcome != nil {
+		stderrMsg.WriteString(fmt.Sprintf("Stderr:\n%s\n", outcome.Error()))
+
+		return fmt.Errorf("Failed to run load test: %v.", outcome)
+	}
+
+	if len(ts.Individual) > 0 || len(ts.General) > 0 {
+		if err := ts.parseStats(ctx, stdoutMsg, stderrMsg, transport); err != nil {
+			stderrMsg.WriteString(fmt.Sprintf("Stderr:\n%s\n", err.Error()))
+			return fmt.Errorf("Failed to parse cpu stats: %v.", err)
+		}
+	}
+
+	if outcome == nil {
+		outcome = proc.Wait(ctx)
+	}
+
+	stdout, stderr := getOutputFromReader(stdoutPipe, stderrPipe)
+
+	if outcome != nil {
+		stderrMsg.WriteString(fmt.Sprintf("Stderr:\n%s\n", string(stderr)))
+
+		return fmt.Errorf("Failed to run load test: %v.", outcome)
+	}
+
+	stdoutMsg.WriteString(fmt.Sprintf("Stdout:\n%s\n", string(stdout)))
+
+	return err
+}
+
+func (ts *TestStep) parseStats(ctx xcontext.Context, stdoutMsg, stderrMsg *strings.Builder, transport transport.Transport) error {
+	duration, err := time.ParseDuration(ts.Parameter.Duration)
+	if err != nil {
+		return fmt.Errorf("wrong interval statement, valid units are ns, us, ms, s, m and h")
+	}
+
+	// Run cpu stats only for 96% of the load duration.
+	interval := duration * 96 / 100
+	offset := duration * 2 / 100
+
+	// Start the cpu stats command with a small offset, so it is in the middle of the load duration.
+	time.Sleep(offset)
+
+	args := []string{
+		ts.Parameter.ToolPath,
+		cmd,
+		argument,
+		fmt.Sprintf("--interval=%s", interval.String()),
+		jsonFlag,
+	}
+
+	proc, err := transport.NewProcess(ctx, privileged, args, "")
+	if err != nil {
+		return fmt.Errorf("Failed to create proc: %w", err)
+	}
+
+	writeCommand(proc.String(), stdoutMsg, stderrMsg)
+
+	stdoutPipe, err := proc.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("Failed to pipe stdout: %v", err)
+	}
+
+	stderrPipe, err := proc.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("Failed to pipe stderr: %v", err)
+	}
+
+	// try to start the process, if that succeeds then the outcome is the result of
+	// waiting on the process for its result; this way there's a semantic difference
+	// between "an error occured while launching" and "this was the outcome of the execution"
+	outcome := proc.Start(ctx)
+	if outcome == nil {
+		outcome = proc.Wait(ctx)
+	}
+	if outcome != nil {
+		return fmt.Errorf("Failed to get CPU stats: %v.", outcome)
+	}
+
+	stdout, stderr := getOutputFromReader(stdoutPipe, stderrPipe)
+
+	stdoutMsg.WriteString(fmt.Sprintf("Stats Stdout:\n%s\n", string(stdout)))
+	stderrMsg.WriteString(fmt.Sprintf("Stats Stderr:\n%s\n", string(stderr)))
+
+	if err = ts.parseOutput(ctx, stdoutMsg, stderrMsg, stdout); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// getOutputFromReader reads data from the provided io.Reader instances
+// representing stdout and stderr, and returns the collected output as byte slices.
+func getOutputFromReader(stdout, stderr io.Reader) ([]byte, []byte) {
+	// Read from the stdout and stderr pipe readers``
+	outBuffer, err := readBuffer(stdout)
+	if err != nil {
+		fmt.Printf("Failed to read from Stdout buffer: %v\n", err)
+	}
+
+	errBuffer, err := readBuffer(stderr)
+	if err != nil {
+		fmt.Printf("Failed to read from Stderr buffer: %v\n", err)
+	}
+
+	return outBuffer, errBuffer
+}
+
+// readBuffer reads data from the provided io.Reader and returns it as a byte slice.
+// It dynamically accumulates the data using a bytes.Buffer.
+func readBuffer(r io.Reader) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	if _, err := io.Copy(buf, r); err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (ts *TestStep) parseOutput(ctx xcontext.Context, stdoutMsg *strings.Builder, stderrMsg *strings.Builder,
+	stdout []byte,
+) error {
+	var (
+		stats      cpu.Stats
+		finalError bool
+	)
+
+	if len(stdout) != 0 {
+		if err := json.Unmarshal(stdout, &stats); err != nil {
+			return fmt.Errorf("failed to unmarshal stdout: %v", err)
+		}
+	}
+
+	for _, expect := range ts.expectStepParams.General {
+		if err := stats.CheckGeneralOption(expect, stdoutMsg, stderrMsg); err != nil {
+			finalError = true
+		}
+	}
+
+	for _, expect := range ts.expectStepParams.Individual {
+		if err := stats.CheckIndividualOption(expect, true, stdoutMsg, stderrMsg); err != nil {
+			finalError = true
+		}
+	}
+
+	if finalError {
+		return fmt.Errorf("Some expect options are not as expected.")
+	}
+
+	return nil
+}
